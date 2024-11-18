b8ee28aa295045342e27f1a5b6a0052a5e24e731
Alex Tindall <tindall.al@gmail.com>
2024-11-18 14:19:40 -0600

    created sort for items

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,5 +1,4 @@
 <template>
-
   <q-header elevated>
     <q-toolbar style="height: 100px">
       <q-btn
@@ -17,7 +16,7 @@
   <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
     <q-list>
       <q-item v-for="hero in filteredHeros" :key="hero.id">
-        <q-card class="my-card" flat bordered>
+        <q-card class="hero" flat bordered>
           <q-card-section horizontal>
             <q-img
               class="col hero-icon-img"
@@ -39,10 +38,10 @@
           v-model="tab"
           dense
           class="text-grey"
-          active-color="light"  
+          active-color="light"
           align="justify"
           narrow-indicator
-          style="height: 50px;"
+          style="height: 50px"
         >
           <q-tab name="build" label="Build" />
           <q-tab name="weapons" label="Weapons" />
@@ -53,12 +52,30 @@
         <q-tab-panels v-model="tab" animated style="height: 80vh">
           <q-tab-panel name="build">
             <div class="text-h6">Builder</div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
           </q-tab-panel>
 
           <q-tab-panel name="weapons">
             <div class="text-h6">Weapons</div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            <div class="q-pa-md">
+              <q-list class="row items-center">
+                <q-item
+                  v-for="item in filteredItems"
+                  :key="item.id"
+                  class="col-xl-2 col-lg-2 col-md-3 col-sm-4 col-xs-12"
+                >
+                  <q-card class="item">
+                    <img :src="item.image" class="weapon item-img"/>
+
+                    <q-card-section>
+                      <p class="text-center">{{ item.name }}</p>
+                    </q-card-section>
+
+                    <q-card-section class="q-pt-none">
+                    </q-card-section>
+                  </q-card>
+                </q-item>
+              </q-list>
+            </div>
           </q-tab-panel>
 
           <q-tab-panel name="vitality">
@@ -74,7 +91,6 @@
       </q-card>
     </div>
   </div>
-
 </template>
 
 <script setup>
@@ -92,7 +108,7 @@ const heros = ref([]);
 
 // methods
 async function searchItems() {
-  let slotType = "vitality";
+  let slotType = "weapon";
   let url = "https://assets.deadlock-api.com/v2/items/by-slot-type/" + slotType;
   let config = {
     params: {
@@ -105,6 +121,7 @@ async function searchItems() {
   try {
     const response = await axios.get(url, config);
     items.value = response.data;
+    console.log(items.value)
   } catch (error) {
     console.error("Error fetching data from Deadlock API:", error);
   } finally {
@@ -145,11 +162,21 @@ onMounted(() => {
 
 // Computed
 const filteredHeros = computed(() => {
-  return heros.value.filter((hero) => hero.in_development === false).sort((a, b) => a.name.localeCompare(b.name));
+  return heros.value
+    .filter((hero) => hero.in_development === false)
+    .sort((a, b) => a.name.localeCompare(b.name));
 });
 
 const filteredItems = computed(() => {
-  return heros.value.filter((item) => item.shopable === true);
+  return items.value
+    .filter((item) => item.shopable === true)
+    .sort((a, b) => {
+      const costDifference = a.cost - b.cost;
+      if (costDifference !== 0) {
+        return costDifference;
+      }
+      return a.name.localeCompare(b.name);
+    });
 });
 
 const tab = ref("build"); // Initialize the active tab to "build"
