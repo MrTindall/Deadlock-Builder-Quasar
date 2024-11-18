00b1afa37192782c56beca7c222e742e4b25c1ad
Alex Tindall <tindall.al@gmail.com>
2024-11-18 10:05:40 -0600

    got images to work displaying cards in drawer

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -17,9 +17,12 @@
 
     <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
       <q-list>
-        <q-item v-for="hero in heros" :key="hero.id">
-          <q-card>
-            {{ hero.name }}
+        <q-item v-for="hero in filteredHeros" :key="hero.id">
+          <q-card class="my-card">
+            <img :src= "hero.images.icon_hero_card"/>
+            <q-card-section class="dark-banner">
+              <div class="text-h6 text-center">{{hero.name}}</div>
+            </q-card-section>
           </q-card>
         </q-item>
       </q-list>
@@ -32,8 +35,8 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
-import axios from 'axios';
+import { ref, computed, onMounted } from "vue";
+import axios from "axios";
 
 defineOptions({
   name: "MainLayout",
@@ -49,8 +52,8 @@ async function searchItems() {
   let config = {
     params: {
       slot_type: slotType,
-      language: 'english',
-      client_version: '5349',
+      language: "english",
+      client_version: "5349",
     },
   };
 
@@ -60,7 +63,7 @@ async function searchItems() {
   } catch (error) {
     console.error("Error fetching data from Deadlock API:", error);
   } finally {
-    console.log('Done');
+    console.log("Done");
   }
 }
 
@@ -68,20 +71,20 @@ async function searchHeros() {
   let url = "https://assets.deadlock-api.com/v2/heroes";
   let config = {
     params: {
-      only_active: 'true',
-      language: 'english',
-      client_version: '5368',
+      only_active: "true",
+      language: "english",
+      client_version: "5368",
     },
   };
 
   try {
     const response = await axios.get(url, config);
-    console.log(response)
+    console.log(response.data);
     heros.value = response.data;
   } catch (error) {
     console.error("Error fetching data from Deadlock API:", error);
   } finally {
-    console.log('Done');
+    console.log("Done");
   }
 }
 
@@ -93,5 +96,14 @@ function toggleLeftDrawer() {
 onMounted(() => {
   searchItems();
   searchHeros();
+});
+
+// Correctly define computed property
+const filteredHeros = computed(() => {
+  return heros.value.filter((hero) => hero.needs_testing === false);
+});
+
+const filteredItems = computed(() => {
+  return heros.value.filter((item) => item.shopable === true);
 });
 </script>
