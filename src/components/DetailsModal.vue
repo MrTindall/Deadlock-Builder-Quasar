a5ba4e6e32ce36a1c47604578729e2fc57f67ef0
Alex Tindall <tindall.al@gmail.com>
2024-11-26 13:31:31 -0600

    displaying data for  modal

--- src/components/DetailsModal.vue
+++ src/components/DetailsModal.vue
@@ -6,12 +6,26 @@
         <br />
         ${{ item.cost }}
       </q-card-section>
+
       <q-card-section v-if="item.component_items && item.component_items.length" :class="['row items-center', 'text-h6']">
         Component of: {{ getComponentName(item, itemList) }}
       </q-card-section>
-      <q-card-section :class="['row items-center', 'text-h6', [item.item_slot_type + '-darker']]">
-        Activation: {{ item.activation }}
+
+      <q-card-section v-if="item.activation === 'passive'" :class="['row items-center', 'text-h6', [item.item_slot_type + '-darker']]">
+        Passive
       </q-card-section>
+      <q-card-section v-else :class="['row items-center', 'text-h6', [item.item_slot_type + '-darker']]">
+        Active
+      </q-card-section>
+
+      <div class="q-pa-md">
+        <div v-for="(property, key) in item.properties" :key="key" class="property">
+          <!-- Exclude 'AbilityUnitTargetLimit' -->
+          <div v-if="property.value > 0 && key !== 'AbilityUnitTargetLimit'" class="property-row">
+            {{ formatKey(key) }}: {{ formatValue(property) }}
+          </div>
+        </div>
+      </div>
 
       <q-card-actions class="flex justify-end">
         <div>
@@ -21,6 +35,8 @@
     </q-card>
   </q-dialog>
 </template>
+
+
 
 <script>
 export default {
@@ -39,18 +55,36 @@ export default {
     getComponentName(item, arr) {
       if (item.component_items && item.component_items.length > 0) {
         const componentName = item.component_items[0];
-        console.log(componentName)
         const foundItem = arr.find((element) => 
           element.class_name === componentName
         );
-        
         return foundItem ? foundItem.name : "Unknown Component";
       }
       return "No component";
     },
+    formatKey(key) {
+      // Format the property key to display it nicely (e.g., camelCase -> "Camel Case")
+      return key.replace(/([A-Z])/g, ' $1').replace(/^./, str => str.toUpperCase());
+    },
+    formatValue(property) {
+      // Handle formatting for different property values
+      if (property.display_units) {
+        return `${property.value} ${this.getDisplayUnit(property.display_units)}`;
+      }
+      return property.value;
+    },
+    getDisplayUnit(unit) {
+      const unitMap = {
+        "EDisplayUnit_Meters": "m",
+        "EDisplayUnit_MetersPerSecond": "m/s",
+        "EDisplayUnit_Normal": "",
+      };
+      return unitMap[unit] || '';
+    }
   },
 };
 </script>
+
 <style scoped lang="scss">
 .q-card {
   width: 600px;
