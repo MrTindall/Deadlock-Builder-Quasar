2255d81280dd5238ebd47cf55fd3957a41f3a32c
Alex <tindall.al@gmail.com>
2024-12-07 12:08:20 -0600

    fixed saveBuild to check if build exist, if so then replace build else just save new build

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -375,8 +375,9 @@ function saveBuild() {
   const buildArr = allItems.value.filter(item => item.isActive === true);
   const currentBuild = characterBuilds?.find((item) => model.value.value === item.buildName && model.value.value.toLowerCase() !== 'new build')
   if(currentBuild) {
-      console.log(currentBuild)
-      
+      let tempBuildName = currentBuild.buildName;
+      characterBuilds?.splice((item) => model.value.value === item.buildName && model.value.value.toLowerCase() !== 'new build')
+      characterBuilds.push(new Build(selectedHero.value, tempBuildName, buildArr))
     } else {
       characterBuilds.push(new Build(selectedHero.value, buildName.value, buildArr))
     }
